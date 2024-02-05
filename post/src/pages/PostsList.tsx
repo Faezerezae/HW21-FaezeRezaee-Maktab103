ca333f0d805d98cef75f38e1d18c4be6fd96cb47
Faezerezae <faezerezae234@gmail.com>
2024-02-05 16:37:22 +0330

    rename PostList

--- post/src/pages/PostsList.tsx
+++ post/src/pages/PostsList.tsx
@@ -5,7 +5,7 @@ import {CardPost} from "../components/CardPost";
 import { PostSkeleton } from "../components/PostSkeleton";
 import { fetchPosts } from "../apis/post-api";
 
-export default function Post() {
+export default function PostsList() {
   const { data,isLoading } = useQuery("Post", fetchPosts);
   console.log(data)
 
@@ -27,7 +27,9 @@ export default function Post() {
       <div className=" grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 my-2 mx-auto max-w-7xl w-full px-4 sm:px-6 h-[598px] overflow-y-scroll lg:px-8 items-center gap-2 ">
         {data?.posts?.map((el: any) => {
           return(
-          <CardPost {...el}  key={el.id}/>
+            <div className="rounded shadow">
+          <CardPost post={el}/>
+          </div>
           ) 
         })}
       </div>
